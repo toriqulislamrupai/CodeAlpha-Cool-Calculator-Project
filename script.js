69d4d2664e39267c818348798db1a717b83aaa1d
toriqulislamrupai <toriqulislamrupai@gmail.com>
2024-09-04 14:48:42 +0600

    adding my cool calculator data

---
+++ script.js
@@ -0,0 +1,38 @@
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+    if (display.innerText === '0') {
+        display.innerText = number;
+    } else {
+        display.innerText += number;
+    }
+}
+
+function appendOperator(operator) {
+    const lastChar = display.innerText.slice(-1);
+    if (['+', '-', '*', '/'].includes(lastChar)) {
+        display.innerText = display.innerText.slice(0, -1) + operator;
+    } else {
+        display.innerText += operator;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length === 1) {
+        display.innerText = '0';
+    } else {
+        display.innerText = display.innerText.slice(0, -1);
+    }
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
